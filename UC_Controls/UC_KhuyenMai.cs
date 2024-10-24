1ea77c1b0097c5087187205daf2c56753cb515be
vantu2004 <tulevan526@gmail.com>
2024-10-24 23:40:47 +0700

    init

---
+++ UC_Controls/UC_KhuyenMai.cs
@@ -0,0 +1,47 @@
+﻿using Nhom11.DB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nhom11
+{
+    public partial class UC_KhuyenMai : UserControl
+    {
+        KhuyenMaiDAO khuyenMaiDAO = new KhuyenMaiDAO();
+
+        public UC_KhuyenMai()
+        {
+            InitializeComponent();
+            LoadDanhSachMaKhuyenMai();
+        }
+
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            form_SuaMaKhuyenMai form_SuaMaKhuyenMai = new form_SuaMaKhuyenMai();
+
+            form_SuaMaKhuyenMai.ShowDialog();
+        }
+
+        private void LoadDanhSachMaKhuyenMai()
+        {
+            try
+            {
+                // Lấy dữ liệu từ view
+                DataTable dt = khuyenMaiDAO.GetDanhSachMaKhuyenMai();
+                // Gán dữ liệu vào DataGridView
+                dgv_DanhSachMaKhuyenMai.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi nếu có
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ UC_Controls/UC_SanPham.cs
@@ -0,0 +1,65 @@
+﻿using Nhom11.DB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nhom11
+{
+    public partial class UC_SanPham : UserControl
+    {
+        SanPhamDAO sanPhamDAO = new SanPhamDAO();
+        public UC_SanPham()
+        {
+            InitializeComponent();
+            LoadDanhSachDienThoaiCoSan();
+            LoadDanhSachDienThoaiCanCapNhat();
+        }
+
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            form_SuaSanPham form_SuaSanPham = new form_SuaSanPham();
+
+            form_SuaSanPham.ShowDialog();
+        }
+
+        private void LoadDanhSachDienThoaiCoSan()
+        {
+            try
+            {
+                // Lấy dữ liệu từ view
+                DataTable dt = sanPhamDAO.GetDanhSachDienThoaiCoSan();
+                // Gán dữ liệu vào DataGridView
+                dgv_DanhSachSanPham.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi nếu có
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+
+        }
+
+        private void LoadDanhSachDienThoaiCanCapNhat()
+        {
+            try
+            {
+                // Lấy dữ liệu từ view
+                DataTable dt = sanPhamDAO.GetDanhSachDienThoaiCanCapNhat();
+                // Gán dữ liệu vào DataGridView
+                dgv_SamPhamCanCapNhat.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi nếu có
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+
+        }
+    }
+}
